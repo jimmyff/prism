0ee563bc0b92a302208a60f947e94f7ea3e5adf0
jimmyff <jimmy@rocketware.co.uk>
2025-07-31 12:25:36 +0100

    reworked the spectrum palette

--- packages/prism/tools/palettes/sources/spectrum.dart
+++ packages/prism/tools/palettes/sources/spectrum.dart
@@ -1,104 +1,66 @@
 import 'package:prism/prism.dart';
 
-/// Material Design colors palette
-/// Contains all Material Design colors as RayScheme objects
-Map<String, RayScheme<RayWithLuminanceOklch>> spectrumColors = {
-  // Red
-  'red': RayScheme.fromOklch(RayOklch(
-    l: 0.65,
-    c: 0.2268,
-    h: 24.87,
-  )),
-  'pink': RayScheme.fromOklch(RayOklch(
-    l: 0.65,
-    c: 0.2302,
-    h: 5.28,
-  )),
-  'purple': RayScheme.fromOklch(RayOklch(
-    l: 0.65,
-    c: 0.334,
-    h: 321.35,
-  )),
-  'violet': RayScheme.fromOklch(RayOklch(
-    l: 0.65,
-    c: 0.20,
-    h: 297.66,
-  )),
-  'indigo': RayScheme.fromOklch(RayOklch(
-    l: 0.6146,
-    c: 0.2285,
-    h: 279.28,
-  )),
-  'blue': RayScheme.fromOklch(RayOklch(
-    l: 0.65,
-    c: 0.1888,
-    h: 248.81,
-  )),
-  'cyan': RayScheme.fromOklch(RayOklch(
-    l: 0.65,
-    c: 0.1501,
-    h: 210.8169,
-  )),
-  'teal': RayScheme.fromOklch(RayOklch(
-    l: 0.65,
-    c: 0.1561,
-    h: 183.38,
-  )),
-  'green': RayScheme.fromOklch(RayOklch(
-    l: 0.65,
-    c: 0.2,
-    h: 144.21,
-  )),
-  'lime': RayScheme.fromOklch(RayOklch(
-    l: 0.8,
-    c: 0.21,
-    h: 116.06,
-  )),
-  'yellow': RayScheme.fromOklch(
-    RayOklch(
-      l: 0.9116,
-      c: 0.2016,
-      h: 101.6,
-    ),
-    // hueTransform: (double hue, double lightnessDelta) =>
-    //     hue + (lightnessDelta.clamp(-0.2, 0) * 200.0),
-    // chromaTransform: (double chroma, double lightnessDelta) =>
-    //     chroma * (1.0 - lightnessDelta.abs() * 0.5),
-  ),
-  'amber': RayScheme.fromOklch(RayOklch(
-    l: 0.8369,
-    c: 0.1947,
-    h: 84.83,
-  )),
-  'tangerine': RayScheme.fromOklch(RayOklch(
-    l: 0.7941,
-    c: 0.1914,
-    h: 64.05,
-  )),
-  'orange': RayScheme.fromOklch(RayOklch(
-    l: 0.7147,
-    c: 0.2274,
-    h: 46.15,
-  )),
+final base = RayOklch.validated(l: 0.50, c: 0.3, h: 0);
+final offset = 8;
+var structure = [
+  (name: 'pink', lightness: 0.62, bright: false),
+  (name: 'crimson', lightness: 0.6, bright: false),
+  (name: 'red', lightness: 0.61, bright: false),
+  (name: 'rust', lightness: 0.65, bright: false),
+  (name: 'orange', lightness: 0.67, bright: false),
+  (name: 'amber', lightness: 0.83, bright: true),
+  (name: 'yellow', lightness: 0.92, bright: true),
+  (name: 'lemon', lightness: 0.88, bright: true),
+  (name: 'lime', lightness: 0.79, bright: true),
+  (name: 'green', lightness: 0.69, bright: false),
+  (name: 'emerald', lightness: 0.63, bright: false),
+  (name: 'teal', lightness: 0.6, bright: false),
+  (name: 'turquoise', lightness: 0.75, bright: true),
+  (name: 'cyan', lightness: 0.8, bright: true),
+  (name: 'cerulean', lightness: 0.66, bright: false),
+  (name: 'azure', lightness: 0.65, bright: false),
+  (name: 'sky', lightness: 0.63, bright: false),
+  (name: 'blue', lightness: 0.61, bright: false),
+  (name: 'indigo', lightness: 0.55, bright: false),
+  (name: 'berry', lightness: 0.57, bright: false),
+  (name: 'violet', lightness: 0.62, bright: false),
+  (name: 'purple', lightness: 0.62, bright: false),
+  (name: 'fuchsia', lightness: 0.65, bright: true),
+  (name: 'magenta', lightness: 0.61, bright: false),
+];
 
-  'brown': RayScheme.fromOklch(
-    RayRgb.fromHex('#795548').toOklch(),
-    generateAccents: false,
-  ),
-  'gray': RayScheme.fromOklch(
-    RayRgb.fromHex('#9E9E9E').toOklch(),
-    generateAccents: false,
-  ),
-  'blueGray': RayScheme.fromOklch(
-    RayRgb.fromHex('#607D8B').toOklch(),
-    generateAccents: false,
-  ),
-  'black': RayScheme.fromOklch(
-    RayRgb.fromHex('#000000').toOklch(),
-    generateAccents: false,
-  ),
-  'white': RayScheme.fromOklch(
-    RayRgb.fromHex('#FFFFFF').toOklch(),
-    generateAccents: false,
-  ),
-};
+var structureFilled = List.generate(
+    24,
+    (i) => i < structure.length
+        ? structure[i]
+        : (name: "filled_$i", lightness: 0.6, bright: false));
+
+Map<String, RayScheme<RayWithLuminanceOklch>> spectrumColors = Map.fromEntries([
+  ...structureFilled.map((e) => MapEntry(
+      e.name,
+      RayScheme.fromOklch(e.bright
+          ? base
+              .withHue(offset +
+                  (structureFilled.indexOf(e) * (360 / structureFilled.length)))
+              .withLightness(e.lightness)
+          : base.withLightness(e.lightness).withHue(offset +
+              (structureFilled.indexOf(e) * (360 / structureFilled.length)))))),
+  MapEntry(
+      'neutralCool',
+      RayScheme.fromOklch(
+        RayOklch.validated(l: 0.55, c: 0.023, h: 250.0),
+        generateAccents: false,
+      )),
+  MapEntry(
+      'neutral',
+      RayScheme.fromOklch(
+        RayOklch.validated(l: 0.55, c: 0.0, h: 0.0),
+        generateAccents: false,
+      )),
+  MapEntry(
+      'neutralWarm',
+      RayScheme.fromOklch(
+        RayOklch.validated(l: 0.55, c: 0.018, h: 40.0),
+        generateAccents: false,
+      )),
+]);
